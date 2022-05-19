0489b79edc04a135919b7a7743b6333a037b0254
Anders Pearson <anders@thraxil.org>
2022-05-19 11:44:15 +0100

    basic implementation of disable_user

--- tests/test_disable_user.py
+++ tests/test_disable_user.py
@@ -1,7 +1,10 @@
 from io import StringIO
 
+from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.test import TestCase
+
+from .factories import UserFactory
 
 
 class TestDisableUser(TestCase):
@@ -18,5 +21,21 @@ class TestDisableUser(TestCase):
 
     def test_command_exists(self):
         """just make sure the command even exists"""
-        out = self.call_command()
-        self.assertNotEqual(out, "")
+        self.call_command("dummy")
+
+    def test_disables_active_user(self):
+        u = UserFactory(is_active=True)
+        self.call_command(u.username)
+
+        # check that they are no longer active
+        r = get_user_model().objects.get(username=u.username)
+        self.assertFalse(r.is_active)
+
+    def test_already_inactive_user(self):
+        """this should just be a no-op"""
+        u = UserFactory(is_active=False)
+        self.call_command(u.username)
+
+        # check that they are no longer active
+        r = get_user_model().objects.get(username=u.username)
+        self.assertFalse(r.is_active)
